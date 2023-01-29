a34b07b4bc8b039ba895c681c0d9460c79ecd11f
Kishan Kumar Majeti <quic_kkmajeti@quicinc.com>
2023-01-29 19:37:17 +0530

    asoc: Add compress volume and mute support
    Add volume and mute support for AMR streams
    
    Change-Id: I514d58de496ae83c976bb72272dd891da663c817
    Signed-off-by: Kishan Kumar Majeti <quic_kkmajeti@quicinc.com>
    (cherry picked from commit 5f572c7444dd631802f5b57c908e89e4ac82e93f)
    (cherry picked from commit dbf5e379dded6929e1845a0cbc0795f50372be1d)

--- asoc/msm-compress-q6-v2.c
+++ asoc/msm-compress-q6-v2.c
@@ -445,6 +445,18 @@ static int msm_compr_set_volume(struct snd_compr_stream *cstream,
 		gain_list[0] = volume_l;
 		gain_list[1] = volume_r;
 		gain_list[2] = volume_l;
+
+		switch (prtd->codec) {
+		case FORMAT_AMRNB:
+		case FORMAT_AMRWB:
+		case FORMAT_AMR_WB_PLUS:
+			use_default = true; // For AMR formats using default format as there is no format data
+			break;
+
+		default:
+			break;
+		}
+
 		if (use_default)
 			num_channels = 3;
 		rc = q6asm_set_multich_gain(prtd->audio_client, num_channels,
