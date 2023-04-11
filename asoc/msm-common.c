86a2bbd01a04356aa88651cd109e18f0ebd177d5
Meghana Reddy Mula <quic_mmula@quicinc.com>
2023-04-11 14:08:43 +0530

    asoc: msm_common: add rw perm on card_state
    add RW-RW-RW- permission to the card_state for SSR.
    
    Change-Id: Ideb65bc8f88aa700c9b5ca80b3062b0e8bae4775
    Signed-off-by: Meghana Reddy Mula <quic_mmula@quicinc.com>

--- asoc/msm-common.c
+++ asoc/msm-common.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/platform_device.h>
@@ -23,7 +23,7 @@ struct snd_card_pdata *snd_card_pdata;
 
 static struct attribute card_state_attr = {
 	.name = "card_state",
-	.mode = 0660,
+	.mode = 0666,
 };
 
 int snd_card_notify_user(snd_card_status_t card_status)
