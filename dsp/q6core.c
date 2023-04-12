191f110366c74edb51dca67988c96d2f2247cbb1
Sandhya Mutha Naga Venkata <quic_smuthana@quicinc.com>
2023-04-12 12:34:26 +0530

    dsp: q6core: Avoid OOB access in q6core
    "num_services", a signed integer when compared
    with constant results in conversion of signed integer
    to max possible unsigned int value when "num_services"
    is a negative value. This can lead to OOB read.
    Fix is to handle this case.
    
    Change-Id: Id6a8f150d9019c972a87f789e4c626337a97bfff
    Signed-off-by: Soumya Managoli <quic_c_smanag@quicinc.com>
    (cherry picked from commit 521c1989862591128e3be137d6dd3cc03cc9a6ca)

--- dsp/q6core.c
+++ dsp/q6core.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/kernel.h>
@@ -271,7 +272,7 @@ static int update_key_data(uint32_t *payload)
 static int parse_fwk_version_info(uint32_t *payload, uint16_t payload_size)
 {
 	size_t ver_size;
-	int num_services;
+	uint16_t num_services;
 
 	pr_debug("%s: Payload info num services %d\n",
 		 __func__, payload[4]);
