89218fd8834f2de7fe6c5c8cec41dc4921b51a14
Thrithendra Ungarala <quic_tungaral@quicinc.com>
2023-07-25 12:08:39 +0530

    ASoC: msm-pcm-voip: Fixing error message in voip_process_ul_pkt.
    This commit fixes an error in the error handling code
    where the wrong argument was used in the pr_err() call.
    Added check for interger underflow.
    
    Change-Id: Ic8f5c70f8aaca5936814d0e2a3dc925f4e3ac5aa
    (cherry picked from commit 191f6f8f79ac263352d88bddf67c361513ebd1ce)
    (cherry picked from commit 8886e5ff40655191aa53dc7eab8aeb745dc0abd5)

--- asoc/msm-pcm-voip-v2.c
+++ asoc/msm-pcm-voip-v2.c
@@ -375,7 +375,7 @@ static void voip_process_ul_pkt(uint8_t *voc_pkt,
 		case MODE_AMR: {
 			if (pkt_len <= DSP_FRAME_HDR_LEN) {
 				pr_err("%s: pkt_len %d is < required len\n",
-						pkt_len);
+						__func__, pkt_len);
 				spin_unlock_irqrestore(&prtd->dsp_ul_lock,
 							dsp_flags);
 				return;
@@ -402,7 +402,7 @@ static void voip_process_ul_pkt(uint8_t *voc_pkt,
 		case MODE_4GV_NW: {
 			if (pkt_len <= DSP_FRAME_HDR_LEN) {
 				pr_err("%s: pkt_len %d is < required len\n",
-						pkt_len);
+						__func__, pkt_len);
 				spin_unlock_irqrestore(&prtd->dsp_ul_lock,
 							dsp_flags);
 				return;
@@ -446,7 +446,7 @@ static void voip_process_ul_pkt(uint8_t *voc_pkt,
 
 			if (pkt_len <= 2 * DSP_FRAME_HDR_LEN) {
 				pr_err("%s: pkt_len %d is < required len\n",
-						pkt_len);
+						__func__, pkt_len);
 				spin_unlock_irqrestore(&prtd->dsp_ul_lock,
 							dsp_flags);
 				return;
@@ -485,6 +485,14 @@ static void voip_process_ul_pkt(uint8_t *voc_pkt,
 							(*voc_pkt) & 0x03;
 				buf_node->frame.frm_hdr.timestamp = timestamp;
 				voc_pkt = voc_pkt + DSP_FRAME_HDR_LEN;
+
+				if (pkt_len <= 2 * DSP_FRAME_HDR_LEN) {
+					pr_err("%s: pkt_len %d is < required len\n",
+								__func__, pkt_len);
+					spin_unlock_irqrestore(&prtd->dsp_ul_lock,
+									dsp_flags);
+				return;
+				}
 
 				/* There are two frames in the buffer. Length
 				 * of the second frame:
