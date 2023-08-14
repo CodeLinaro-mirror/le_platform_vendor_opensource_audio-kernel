bfe1f397f7f75757bfa1bff2a8649c126deccb62
npaladug <quic_npaladug@quicinc.com>
2023-08-14 21:47:05 +0530

    APR: Fix for deadlock in q6core during subsystem restart
    Addition of initial boot check to avoid deadlock in q6core
    during subsystem restart.
    
    Change-Id: I4dbf18eb5a19961c9261b34e817943fdf1336a25
    Signed-off-by: Nagarjuna Paladugu <quic_npaladug@quicinc.com>

--- ipc/apr.c
+++ ipc/apr.c
@@ -39,6 +39,7 @@
 #define APR_PKT_IPC_LOG_PAGE_CNT 2
 
 static int apr_pkt_cnt_adsp_restart = 20;
+static int apr_reg_initial_bootup = true;
 module_param(apr_pkt_cnt_adsp_restart, int, 0664);
 MODULE_PARM_DESC(apr_pkt_cnt_adsp_restart, "set apr pktcount for adsp restart feature");
 
@@ -517,17 +518,25 @@ struct apr_svc *apr_register(char *dest, char *svc_name, apr_fn svc_fn,
 
 	if (dest_id == APR_DEST_QDSP6) {
 		if (apr_get_q6_state() != APR_SUBSYS_LOADED) {
-			int rt = rproc_boot(rproc_h);
+			int rt;
 
+			if (!apr_reg_initial_bootup) {
+				pr_err_ratelimited("%s: ADSP is not UP", __func__);
+				return NULL;
+			}
+			rt = rproc_boot(rproc_h);
 			if (rt != 0) {
 				pr_err("%s: adsp not up rproc is NULL\n", __func__);
 				return NULL;
+			} else {
+				apr_adsp_up();
+				pr_info("%s:adsp Up,updated adsp status using rproc boot status\n"
+					, __func__);
+				spin_lock(&apr_priv->apr_lock);
+				apr_priv->is_initial_boot = false;
+				spin_unlock(&apr_priv->apr_lock);
+				apr_reg_initial_bootup = false;
 			}
-			apr_adsp_up();
-			pr_info("%s: Updated adsp state up as rproc boot done\n", __func__);
-			spin_lock(&apr_priv->apr_lock);
-			apr_priv->is_initial_boot = false;
-			spin_unlock(&apr_priv->apr_lock);
 		}
 		pr_debug("%s: adsp Up\n", __func__);
 	} else if (dest_id == APR_DEST_MODEM) {
@@ -1121,6 +1130,7 @@ static int apr_notifier_service_cb(struct notifier_block *this,
 		spin_lock(&apr_priv->apr_lock);
 		apr_priv->is_initial_boot = false;
 		spin_unlock(&apr_priv->apr_lock);
+		apr_reg_initial_bootup = false;
 		break;
 	default:
 		break;
@@ -1190,6 +1200,14 @@ static int apr_probe(struct platform_device *pdev)
 	if (!apr_priv)
 		return -ENOMEM;
 
+	apr_priv->dev = &pdev->dev;
+	spin_lock_init(&apr_priv->apr_lock);
+	INIT_WORK(&apr_priv->add_chld_dev_work, apr_add_child_devices);
+
+	spin_lock(&apr_priv->apr_lock);
+	apr_priv->is_initial_boot = true;
+	spin_unlock(&apr_priv->apr_lock);
+
 	prop = of_find_property(pdev->dev.of_node, "qcom,rproc-handle", &size);
 	if (!prop) {
 		dev_err(&pdev->dev, "Missing remotproc handle\n");
@@ -1204,10 +1222,6 @@ static int apr_probe(struct platform_device *pdev)
 		ret = -EPROBE_DEFER;
 		return ret;
 	}
-
-	apr_priv->dev = &pdev->dev;
-	spin_lock_init(&apr_priv->apr_lock);
-	INIT_WORK(&apr_priv->add_chld_dev_work, apr_add_child_devices);
 
 	for (i = 0; i < APR_DEST_MAX; i++)
 		for (j = 0; j < APR_CLIENT_MAX; j++) {
@@ -1230,9 +1244,6 @@ static int apr_probe(struct platform_device *pdev)
 	if (!apr_pkt_ctx)
 		pr_err("%s: Unable to create ipc log context\n", __func__);
 
-	spin_lock(&apr_priv->apr_lock);
-	apr_priv->is_initial_boot = true;
-	spin_unlock(&apr_priv->apr_lock);
 	ret = of_property_read_string(pdev->dev.of_node,
 				      "qcom,subsys-name",
 				      (const char **)(&subsys_name));
