b5d0e7a13cbbc19d967c1c7354ac1b296b1211d9
Thrithendra Ungarala <quic_tungaral@quicinc.com>
2023-10-11 00:27:55 +0530

    asoc: Fixed traces logs during the bootup
    Removed kernel traces from bootup logs.
    
    Change-Id: I92ac8f8e5fe24d420ad1a192678c203e91676d9e

--- asoc/codecs/wcd9330/wcd9330.c
+++ asoc/codecs/wcd9330/wcd9330.c
@@ -703,8 +703,6 @@ int tomtom_enable_qfuse_sensing(struct snd_soc_component *component)
 	 * before checking the status.
 	 */
 	usleep_range(5000, 5500);
-	if ((snd_soc_component_read32(component, TOMTOM_A_QFUSE_STATUS) & (0x03)) != 0x03)
-		WARN(1, "%s: Qfuse sense is not complete\n", __func__);
 
 	if (tomtom->wcd_ext_clk)
 		tomtom_codec_mclk_enable(component, false, false);
