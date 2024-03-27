b5282dca3265cdf9485c6d5b408c206fc1d6cbbc
Samarpanarao Pedapudi <quic_spedapud@quicinc.com>
2024-03-27 15:16:50 +0530

    uapi: correct usage of nonstandard prefix for uapi headers
    - modify header files required for audio kernel and audiohal
      compilation.
    
    Change-Id: Ib7b88f636b037cf4fae80e18e9fa7a03105834fc
    Signed-off-by: Samarpanarao Pedapudi <quic_spedapud@quicinc.com>

--- include/uapi/sound/devdep_params.h
+++ include/uapi/sound/devdep_params.h
@@ -1,3 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only WITH Linux-syscall-note */
+/* Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
 #ifndef _DEV_DEP_H
 #define _DEV_DEP_H
 
@@ -9,7 +13,7 @@ struct dolby_param_data {
 	__s32 be_id;
 	__s32 param_id;
 	__s32 length;
-	__s32 __user *data;
+	__s32 *data;
 };
 
 struct dolby_param_license {
@@ -53,7 +57,7 @@ struct dts_eagle_param_desc {
 	__u32 size;
 	__s32 offset;
 	__u32 device;
-} __packed;
+} __attribute__((packed));
 
 #define HWDEP_FE_BASE                   3000 /*unique base for FE hw dep nodes*/
 struct snd_pcm_mmap_fd {
