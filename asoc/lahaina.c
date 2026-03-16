44647fe02854256da39dcd178def085e6b121f32
Subhadra Jagadeesan <subhjaga@qti.qualcomm.com>
2026-03-16 13:46:28 +0530

    asoc: lahaina: Remove unsupported no_host dai prop
    Remove the no_host dai link property for
    voicemm mode as not supported in snd soc.
    
    Change-Id: I335fb659852b829dfdef4675c52d5c93ae138cb5

--- asoc/lahaina.c
+++ asoc/lahaina.c
@@ -5921,7 +5921,6 @@ static struct snd_soc_dai_link msm_common_dai_links[] = {
 			    SND_SOC_DPCM_TRIGGER_POST},
 		.ignore_suspend = 1,
 		.ignore_pmdown_time = 1,
-		.no_host_mode=1,  /* Fix for hostless Voiccall*/
 		.id = MSM_FRONTEND_DAI_VOICEMMODE1,
 		SND_SOC_DAILINK_REG(voicemmode1),
 	},
@@ -6083,7 +6082,6 @@ static struct snd_soc_dai_link msm_common_dai_links[] = {
 		.dynamic = 1,
 		.dpcm_playback = 1,
 		.dpcm_capture = 1,
-		.no_host_mode=1,  /* Fix for hostless Voiccall*/
 		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
 			    SND_SOC_DPCM_TRIGGER_POST},
 		.ignore_suspend = 1,
